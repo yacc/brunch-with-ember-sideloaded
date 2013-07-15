9e46bba1f57ed2becc2d07c767dee429a29c979a
Ken Snyder <ken@ken.net>
2013-07-15 15:55:22 +0100

    added app.'s

---
+++ app/app.js
@@ -0,0 +1,5 @@
+// Application bootstrapper
+window.App = Ember.Application.create({
+  LOG_TRANSITIONS: true, // basic logging of successful transitions
+  LOG_TRANSITIONS_INTERNAL: true // detailed logging of all routing steps
+});
