b2ca2e937e7f09ad5eb0c258b86599f6be9f48db
Ken Snyder <ken@ken.net>
2013-09-06 16:48:31 +0100

    Added even more verbose debugging including for promises

--- app/app.js
+++ app/app.js
@@ -1,5 +1,21 @@
 // Application bootstrapper
 module.exports = App = Ember.Application.create({
   LOG_TRANSITIONS: true, // basic logging of successful transitions
-  LOG_TRANSITIONS_INTERNAL: true // detailed logging of all routing steps
+  LOG_TRANSITIONS_INTERNAL: true, // detailed logging of all routing steps
+  LOG_STACKTRACE_ON_DEPRECATION : true,
+  LOG_BINDINGS                  : true,
+  LOG_VIEW_LOOKUPS              : true,
+  LOG_ACTIVE_GENERATION         : true 
 });
+
+Ember.RSVP.configure('onerror', function(e) {
+  console.log(e.message); 
+  console.log(e.stack);
+});
+
+function ev(emberId){
+  return Ember.View.views['ember' + emberId];
+}
+function ec(emberId){
+  return ev(emberId).get('controller');
+}
